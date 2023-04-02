117561c338ff7edf3c343c0387af250e7dae4fee
Karasowl <adventismael@gmail.com>
2023-04-02 12:42:27 -0400

    Spotify made js

--- imgCarrousel.js
+++ imgCarrousel.js
@@ -1,9 +1,17 @@
 /* Para que el perfil cambie de imagen con una transición de opacidad */
-const img = document.querySelector('.profile img');
-const images = ['assets/channels4_profile.webp', 'assets/profile.webp', 'assets/35_n.webp', 'assets/27_n.webp', 'assets/112_n.webp', 'assets/295_n.webp', 'assets/612_n.webp' ]; // array de imágenes
+const img = document.querySelector(".profile img");
+const images = [
+  "assets/channels4_profile.webp",
+  "assets/profile.webp",
+  "assets/35_n.webp",
+  "assets/27_n.webp",
+  "assets/112_n.webp",
+  "assets/295_n.webp",
+  "assets/612_n.webp",
+]; // array de imágenes
 
 // Precargar las imágenes
-const preloadImages = images.map(image => {
+const preloadImages = images.map((image) => {
   const img = new Image();
   img.src = image;
   return img;
@@ -11,28 +19,30 @@ const preloadImages = images.map(image => {
 
 let i = 1; // contador
 function cambiarImagen() {
-    img.style.opacity = 0;
-    setTimeout(() => {
-      img.src = images[i];
-      img.style.opacity = 1;
-      i++;
-      if (i >= images.length) {
-        i = 0;
-      }
-    }, 750);
+  img.style.opacity = 0;
+  setTimeout(() => {
+    img.src = images[i];
+    img.style.opacity = 1;
+    i++;
+    if (i >= images.length) {
+      i = 0;
+    }
+  }, 750);
 }
 
 // Función asíncrona para esperar a que todas las imágenes se hayan cargado
 async function waitForImagesToLoad() {
-  await Promise.all(preloadImages.map(img => {
-    if (img.complete) return Promise.resolve();
-    return new Promise(resolve => { img.onload = resolve; });
-  }));
+  await Promise.all(
+    preloadImages.map((img) => {
+      if (img.complete) return Promise.resolve();
+      return new Promise((resolve) => {
+        img.onload = resolve;
+      });
+    })
+  );
 }
 
 // Esperar a que todas las imágenes se hayan cargado antes de ejecutar la función cambiarImagen
 waitForImagesToLoad().then(() => {
   setInterval(cambiarImagen, 5000);
 });
-
-
--- ipapiCuba.js
+++ ipapiCuba.js
@@ -1,9 +1,20 @@
 /* Para que el elemento embebido de Spotify se oculte para usuarios que visitan la página desde Cuba */
-fetch('https://ipapi.co/json/')
-.then(response => response.json())
-.then(data => {
-  if (data.country_name === 'Cuba') {
-    const spotifyPlayer = document.querySelector('.spotify-player');
-    spotifyPlayer.style.display = 'none';
-  }
-});
+fetch("https://ipapi.co/json/")
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.country_name === "Cuba") {
+      const spotifyPlayer = document.querySelector(".spotify-player");
+      spotifyPlayer.style.display = "none";
+    } else {
+      const spotifyPlayer = document.querySelector(".spotify-player");
+      const iframe = document.createElement("iframe");
+      iframe.src =
+        "https://open.spotify.com/embed/track/3cWkIYNstZUq6vmzZbnUXo?utm_source=generator";
+      iframe.frameborder = "0";
+      iframe.allowFullscreen = "";
+      iframe.allow =
+        "autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture";
+      iframe.loading = "lazy";
+      spotifyPlayer.appendChild(iframe);
+    }
+  });
