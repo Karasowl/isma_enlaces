ac9feec17ef4fa7fc63a1fff280236b8618c3292
Karasowl <adventismael@gmail.com>
2023-04-01 20:34:23 -0400

    Await img cache

--- main.js
+++ main.js
@@ -2,6 +2,13 @@
 const img = document.querySelector('.profile img');
 const images = ['assets/channels4_profile.webp', 'assets/profile.webp', 'assets/35_n.webp', 'assets/27_n.webp', 'assets/112_n.webp', 'assets/295_n.webp', 'assets/612_n.webp' ]; // array de imágenes
 
+// Precargar las imágenes
+const preloadImages = images.map(image => {
+  const img = new Image();
+  img.src = image;
+  return img;
+});
+
 let i = 1; // contador
 function cambiarImagen() {
     img.style.opacity = 0;
@@ -13,8 +20,20 @@ function cambiarImagen() {
         i = 0;
       }
     }, 750);
-  }
+}
+
+// Función asíncrona para esperar a que todas las imágenes se hayan cargado
+async function waitForImagesToLoad() {
+  await Promise.all(preloadImages.map(img => {
+    if (img.complete) return Promise.resolve();
+    return new Promise(resolve => { img.onload = resolve; });
+  }));
+}
+
+// Esperar a que todas las imágenes se hayan cargado antes de ejecutar la función cambiarImagen
+waitForImagesToLoad().then(() => {
   setInterval(cambiarImagen, 5000);
+});
 
 /* convertir el elemento html de clase container en un elemnto que simule el movimiento 3D y que siga el movimiento del mouse */
 const container = document.querySelector('.container');
